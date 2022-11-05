e1fa555c3652e45ec3ee6cb4cede98cb7348de61
Sergei Chekmarev <slimercorp@gmail.com>
2022-11-05 13:10:55 +0400

    Day1

---
+++ src/Day1/Part1.java
@@ -0,0 +1,45 @@
+package Day1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Part1 {
+
+	public static void main(String[] args) {
+		File file = new File(".\\src\\Day1\\data.txt");
+		Scanner scan;
+		
+		try {
+			 scan = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("File is not found");
+			return;
+		}
+		
+		List<Integer> data = new ArrayList<>();
+		
+		while (scan.hasNextInt()) {
+			data.add(scan.nextInt());
+		}
+		
+		getAnswer(data);
+		
+		scan.close();
+	}
+
+	
+	public static void getAnswer(List<Integer> data) {
+		for (int i=0; i<data.size(); i++) {
+			for (int j=i+1; j<data.size(); j++) {
+				if ((data.get(i) + data.get(j)) == 2020) {
+					System.out.println(data.get(i) * data.get(j));
+					return;
+				}
+			}
+		}
+	}
+}
