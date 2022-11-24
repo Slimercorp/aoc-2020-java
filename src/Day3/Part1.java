ff133d8e0fbef42866004a6906bf0adbe3d71ba7
Sergei Chekmarev <slimercorp@gmail.com>
2022-11-24 23:12:40 +0400

    Day 3

---
+++ src/Day3/Part1.java
@@ -0,0 +1,54 @@
+package Day3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Part1 {
+
+	public static void main(String[] args) {
+		File file = new File(".\\src\\Day3\\data.txt");
+		Scanner scan;
+		
+		try {
+			 scan = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("File is not found");
+			return;
+		}
+		
+		List<String> data = new ArrayList<>();
+		
+		while (scan.hasNextLine()) {
+			data.add(scan.nextLine());
+		}
+
+		scan.close();
+		
+		int x = 0;
+		int maxY = data.size();
+		int maxX = data.get(0).length();
+		int counter = 0;
+		for (int y=1; y<maxY; y++) {
+			x = x + 3;
+			
+			if (x >= maxX) {
+				x -= maxX;
+			}
+			
+			if (data.get(y).charAt(x) == '#') {
+				counter++;
+			}
+		}
+		
+		System.out.println(counter);
+
+
+		
+	}
+	
+
+}
